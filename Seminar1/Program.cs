291344ca528c7c699bab470a7f0c466c616bf395
Ruslan Sakulin <ruslansakulin@gmail.com>
2022-11-30 23:02:47 +0300

    all exersises added

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -16,6 +16,7 @@ Console.WriteLine("max = " + num2 + " " + "min = " + num1);
 
 //Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+/*
 Console.Write("Клацните циферку на клавиатуре ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Клацните другую циферку ");
@@ -29,13 +30,39 @@ if (b > a) max = b;
 if (c > b) max = c;
 
 Console.WriteLine ("Из этой троицы наибольшее - " + max);
-
-
+*/
 
 //Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
+/*
+Console.Write("Введите число: ");
+int a  = Convert.ToInt32(Console.ReadLine());
 
-
-
+if (a % 2 == 0)
+{
+    Console.WriteLine("Это число - четное! Ура!");
+}
+else 
+Console.WriteLine("Упс( Это число - нечетное(");
+*/
 
 //Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите некоторое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+while (current <= N)
+{
+    if (N % 2 == 0)
+    {
+    Console.Write(" " + current);
+    current = current + 2;
+    }   
+    else
+    {
+    Console.Write(" " + current);
+    current = current + 2;
+    }
+}
