ef0d9e350a66b06204b0225b96f8e71d6dec56a9
Ruslan Sakulin <ruslansakulin@gmail.com>
2022-12-08 21:47:47 +0300

    seminar 2 HW ready

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -12,86 +12,74 @@ int newNum = CutNumb(num);
 Console.WriteLine($"Цифра посередине: {newNum}");
 */
 //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
 /*
 int CutNumb(int num)
 {
-    if (num >= 100 && num <= 999) 
+    if (num > 999)
     {
-    int sot = num % 10;
-    return sot; 
-    }
-    else
-    {
-    int sot = -1;
-    return sot;
-    }  
-}
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int newNum = CutNumb(num);
-Console.WriteLine($"Третья цифра: {newNum}");
-*/
-/*
-if (num >= 100 && num <= 999) 
+        while (num > 999) 
         {
-        int newNum = num % 10;
-        return newNum; 
+            num = num / 10;
         }
-        else
-        {
-        int newNum = -1;
-        return newNum;
-        }   
-*/
-
-//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-/*
-bool WeekEnd(int num)
-{
-    if (num > 5)
-    {
-    return true;
-    }
-    else
-    {
-    return false;
-    }
-}
-Console.WriteLine("Введите число от 1 до 7: ");
-int num = Convert.ToInt32(Console.ReadLine());
-bool res = WeekEnd(num);
-if (res == true)
-{
-Console.WriteLine("Yes");
-}
-else 
-{
-Console.WriteLine("No");
-}
-*/
-int CutNumb(int num)
-{
-    int sot = num % 10;
-    int bigNum = num / 10;
-    while (num > 999)
-    {
-        num = bigNum;
-        break;
-    } 
+    }    
     if (num >= 100 && num <= 999) 
         {
-            num = sot;
+            num = num % 10;
             return num; 
-        }
+        } 
     else
         {
             num = -1;
             return num;
-        }   
+        }                  
 }
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = CutNumb(num);
+if (res == -1) Console.WriteLine("Здесь нет третьей цифры((");
+else Console.WriteLine($"Третья цифра: {res}");
+*/
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+bool WeekEnd(int num)
+{
+    if (num > 5) return true;
+    return false;
+}
+Console.WriteLine("Введите число от 1 до 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool res = WeekEnd(num);
+if (res == true) Console.WriteLine("Yes");
+else Console.WriteLine("No");
+*/
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+int CutNumb(int num)
+{
+    while (num > 999)
+    {
+        if (num > 999) 
+        {
+            num = num / 10;
+            return num;
+        }
+        if (num >= 100 && num <= 999) 
+        {
+            num = num % 10;
+            return num; 
+        } 
+        else
+        {
+            num = -1;
+            return num;
+        }          
+    }       
+return num;
+}
+
 Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int res = CutNumb(num);
 Console.WriteLine($"Третья цифра: {res}");
-
+*/
