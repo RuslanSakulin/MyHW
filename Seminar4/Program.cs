68d43484391f630f0ad9e1d44cf069a66c2808f4
Ruslan Sakulin <ruslansakulin@gmail.com>
2022-12-10 18:12:20 +0300

    HW seminar 4 first exercize ready

---
+++ Seminar4/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+int ProductAB (int a, int b)
+{
+    int i = 0;
+    int product = 1;
+    while (i < b)
+    {
+        product = product * a;
+        i++;
+    }
+    return product;
+}
+
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int res = ProductAB(a, b);
+Console.WriteLine ($"Число {a} в степени {b} = {res}");
+*/
+
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
